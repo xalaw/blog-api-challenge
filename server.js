fb9d8c4a3967cd0476caf8f3babb575aed40281f
Alvin Law <alvinlaw@gmail.com>
2017-03-15 12:30:35 -0700

    add comment

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 
-
+//START LISTENING TO PORT 8080.
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Blog app is listening on port ${process.env.PORT || 8080}`);
 } );
