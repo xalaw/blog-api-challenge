3c3c7d1bd86fa349936fcce9a7edc30ff0dd8214
Alvin Law <alvinlaw@gmail.com>
2017-03-15 22:58:42 -0700

    setup postRouter file and server file

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+//use router to modularize postRouter.
+const router = express.Router();
+//pass in the modularize router 'postRouter'
+const postRouter = require('postRouter');
 
 const app = express();
 
-
+app.use(morgan('common'));
+//specify the route where the router will use.
+router.use('/blog-posts', postRouter);
 
 //START LISTENING TO PORT 8080.
 app.listen(process.env.PORT || 8080, () => {
-    console.log(`Blog app is listening on port ${process.env.PORT || 8080}`);
+	console.log(`Blog app is listening on port ${process.env.PORT || 8080}`);
 } );
